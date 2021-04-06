30bda937cb297f7b0972191c632989763030bfea
Damien Foulhoux <dfoulhoux@openstudio.fr>
2021-04-06 17:08:20 +0200

    update

--- templates/backOffice/default/app/vite.config.ts
+++ templates/backOffice/default/app/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "^/api/.*": {
-        target: "http://localhost:8080",
+        target: "http://orika.test",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/index_dev.php/api"),
       },
