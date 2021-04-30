497c6b1194dfc44749d5ab56786a8ce2a8267989
Antoine ROYER <aroyer@openstudio.fr>
2021-04-30 15:05:47 +0200

    refactor multi-column + add block group

---
+++ Loop/BlockGroup.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace TheliaBlocks\Loop;
+
+use Propel\Runtime\ActiveQuery\Criteria;
+use Thelia\Core\Template\Element\BaseI18nLoop;
+use Thelia\Core\Template\Element\LoopResult;
+use Thelia\Core\Template\Element\LoopResultRow;
+use Thelia\Core\Template\Element\PropelSearchLoopInterface;
+use Thelia\Core\Template\Loop\Argument\Argument;
+use Thelia\Core\Template\Loop\Argument\ArgumentCollection;
+use Thelia\Type\BooleanOrBothType;
+use TheliaBlocks\Model\BlockGroupQuery;
+
+/**
+ * Class BlockGroup
+ * @package TheliaBlocks\Loop
+ *
+ * @method int[]          getId()
+ * @method string[]       getSlug()
+ * @method bool|string  getVisible()
+ */
+class BlockGroup extends BaseI18nLoop implements PropelSearchLoopInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getArgDefinitions()
+    {
+        return new ArgumentCollection(
+            Argument::createIntListTypeArgument('id'),
+            Argument::createAlphaNumStringListTypeArgument('slug'),
+            Argument::createBooleanOrBothTypeArgument('visible', 1)
+        );
+    }
+
+    /**
+     * this method returns a Propel ModelCriteria.
+     *
+     * @return \Propel\Runtime\ActiveQuery\ModelCriteria
+     */
+    public function buildModelCriteria()
+    {
+        $search = BlockGroupQuery::create();
+
+        $this->configureI18nProcessing($search, ['TITLE', 'JSON_CONTENT']);
+
+        $id = $this->getId();
+
+        if (null !== $id) {
+            $search->filterById($id, Criteria::IN);
+        }
+
+        $slug = $this->getSlug();
+
+        if (null !== $slug) {
+            $search->filterBySlug($slug, Criteria::IN);
+        }
+
+        $visible = $this->getVisible();
+
+        if ($visible !== BooleanOrBothType::ANY) {
+            $search->filterByVisible($visible ? 1 : 0);
+        }
+
+        return $search;
+    }
+
+    public function parseResults(LoopResult $loopResult)
+    {
+        /** @var \TheliaBlocks\Model\BlockGroup  $entry */
+        foreach ($loopResult->getResultDataCollection() as $entry) {
+            $blockRenders = [];
+
+            foreach (json_decode($entry->getVirtualColumn('i18n_JSON_CONTENT'), true) as $block) {
+                $blockRenders[] = $this->container->get("thelia.parser")->render("blocks".DS.$block['type']['id'].".html", $block);
+            }
+            $htmlRender = implode(' ', $blockRenders);
+
+            $content = json_decode($entry->getVirtualColumn('i18n_JSON_CONTENT'), true);
+            
+            $row = new LoopResultRow($entry);
+            $row
+                ->set('ID', $entry->getId())
+                ->set('SLUG', $entry->getSlug())
+                ->set('VISIBLE', $entry->getVisible())
+                ->set('TITLE', $entry->getVirtualColumn('i18n_TITLE'))
+                ->set('CONTENT', $content)
+                ->set('RENDER', $htmlRender)
+            ;
+
+            $this->addOutputFields($row, $entry);
+
+            $loopResult->addRow($row);
+        }
+
+        return $loopResult;
+    }
+}
--- templates/backOffice/default/app/src/blocks/MultiColumns.tsx
+++ templates/backOffice/default/app/src/blocks/MultiColumns.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { nanoid } from "nanoid";
 import Tippy from "@tippyjs/react";
 
@@ -7,15 +7,12 @@ import {
   BlockPluginDefinition,
   BlockModuleComponentProps,
 } from "../types";
-import Block from "../components/Block";
-import { usePlugins, __PLUGINS } from "../pluginManager";
-import BlockWrapper from "../components/BlockWrapper";
-import { getI18nText } from "../utils/i18n";
+import BlockGroup from "./Group";
 
 type MultiColumnsData = Array<ColumnData>;
 type ColumnData = {
   id: string;
-  blocks: IBlock[];
+  group: IBlock;
 };
 export type MultiColumnsComponentProps = {
   data: MultiColumnsData;
@@ -26,122 +23,29 @@ const moduleType = {
 };
 const MIN_COLUMNS = 2;
 const MAX_COLUMNS = 5;
-const emptyColumnData = () => ({
-  id: nanoid(),
-  blocks: [emptyBlockData()],
-});
-const emptyBlockData = () => ({
-  id: nanoid(),
-  type: { id: "", title: { default: "" } },
-  parent: null,
-  data: null,
-});
+const emptyColumnData = () => {
+  const columnId = nanoid();
 
-function EmptyBlock({
-  id,
-  onUpdate,
-  onDelete,
-}: {
-  id: IBlock["id"];
-  onUpdate: BlockModuleComponentProps<MultiColumnsData>["onUpdate"];
-  onDelete: Function | null;
-}) {
-  const blocksLibrary = usePlugins();
-  const [isSettingBlock, setIsSettingBlock] = useState<boolean>(false);
-  return (
-    <div className="flex flex-col flex-grow p-2 mt-2 border border-dashed">
-      {isSettingBlock ? (
-        blocksLibrary
-          .filter((block) => block.type.id !== "multiColumns")
-          .map((block) => (
-            <div
-              key={block.id}
-              className="px-4 py-2 border-b border-white cursor-pointer hover:bg-yellow-100 bg-yellow-50"
-              onClick={() => {
-                onUpdate({
-                  id,
-                  data: block.initialData,
-                  type: block.type,
-                });
-              }}
-            >
-              {getI18nText(block.title)}
-            </div>
-          ))
-      ) : (
-        <div className="flex">
-          <button
-            className="flex-1 p-4 text-xl bg-yellow-50 hover:bg-yellow-100"
-            onClick={() => setIsSettingBlock(true)}
-          >
-            Selectionnez un block
-          </button>
-          {onDelete && (
-            <Tippy content={"Supprimer le bloc"}>
-              <button
-                className="px-4 py-2 ml-2 text-sm text-red-500 bg-red-50 hover:bg-red-100"
-                onClick={() => onDelete()}
-              >
-                <i className="fa fa-trash"></i>
-              </button>
-            </Tippy>
-          )}
-        </div>
-      )}
-    </div>
-  );
-}
+  return {
+    id: columnId,
+    group: {
+      id: nanoid(),
+      type: BlockGroup.type,
+      parent: columnId,
+      data: BlockGroup.initialData,
+    },
+  };
+};
 
 function MultiColumnsComponent({
   data,
-  id,
   onUpdate,
 }: BlockModuleComponentProps<MultiColumnsData>) {
-  const onUpdateEmpty = (newBlock: IBlock) => {
-    onUpdate(
-      data.map((column) => ({
-        ...column,
-        blocks: column.blocks.map((block) => {
-          if (block.id === newBlock.id) {
-            return {
-              ...newBlock,
-              parent: id,
-            };
-          }
-          return block;
-        }),
-      })) as MultiColumnsData
-    );
-  };
-
   const handleDeleteColumn = (columnToDelete: ColumnData) => {
     onUpdate(
       data.filter(
         (column) => column?.id !== columnToDelete.id
       ) as MultiColumnsData
-    );
-  };
-
-  const handleDeleteEmpty = (blockToDelete: IBlock) => {
-    onUpdate(
-      data.map((column) => ({
-        ...column,
-        blocks: column.blocks.filter((block) => block?.id !== blockToDelete.id),
-      })) as MultiColumnsData
-    );
-  };
-
-  const handleDeleteBlock = (blockToDelete: IBlock) => {
-    onUpdate(
-      data.map((column) => ({
-        ...column,
-        blocks: column.blocks.map((block) => {
-          if (block.id === blockToDelete.id) {
-            return emptyBlockData();
-          }
-          return block;
-        }),
-      })) as MultiColumnsData
     );
   };
 
@@ -153,40 +57,21 @@ function MultiColumnsComponent({
     ]);
   };
 
-  const addBlock = (column: ColumnData, index: number) => {
-    onUpdate(
-      data.map((currentCol) => {
-        if (currentCol.id === column.id) {
-          return {
-            ...currentCol,
-            blocks: [
-              ...currentCol.blocks.slice(0, index + 1),
-              emptyBlockData(),
-              ...currentCol.blocks.slice(index + 1),
-            ],
-          };
-        }
-        return currentCol;
-      }) as MultiColumnsData
-    );
-  };
-
-  const handleUpdateBlock = (currentBlock: IBlock) => (
+  const handleUpdateGroup = (currentColumn: ColumnData) => (
     newData: IBlock["data"]
   ) => {
     onUpdate(
-      data.map((column) => ({
-        ...column,
-        blocks: column.blocks.map((block) => {
-          if (block.id === currentBlock.id) {
-            return {
-              ...block,
-              data: newData,
-            };
-          }
-          return block;
-        }),
-      })) as MultiColumnsData
+      data.map((column) =>
+        column.id === currentColumn.id
+          ? {
+              ...column,
+              group: {
+                ...column.group,
+                data: newData,
+              },
+            }
+          : column
+      )
     );
   };
 
@@ -195,44 +80,11 @@ function MultiColumnsComponent({
       {data.map((column, indexColumn) => (
         <React.Fragment key={`${column.id}-wrapper`}>
           <div className="flex flex-col flex-1">
-            {column.blocks.map((block, indexBlock) => (
-              <div className="flex flex-col" key={`${block.id}-wrapper`}>
-                {block.type.id === "" ? (
-                  <EmptyBlock
-                    key={block.id}
-                    id={block.id}
-                    onUpdate={onUpdateEmpty}
-                    onDelete={
-                      column.blocks.length > 1
-                        ? () => handleDeleteEmpty(block)
-                        : null
-                    }
-                  />
-                ) : (
-                  <div key={block.id} className="flex-1 mt-2">
-                    <BlockWrapper
-                      block={block}
-                      canMove={false}
-                      handleDelete={handleDeleteBlock}
-                    >
-                      <Block
-                        block={block}
-                        handleUpdate={handleUpdateBlock(block)}
-                      />
-                    </BlockWrapper>
-                  </div>
-                )}
-                <Tippy content={"Ajouter un bloc"}>
-                  <button
-                    key={`${block.id}-add-col`}
-                    className="p-2 mt-2 border border-dashed bg-gray-50 focus:outline-none hover:bg-gray-100"
-                    onClick={() => addBlock(column, indexBlock)}
-                  >
-                    <i className="fa fa-plus"></i>
-                  </button>
-                </Tippy>
-              </div>
-            ))}
+            <BlockGroup.component
+              data={column.group.data}
+              onUpdate={handleUpdateGroup(column)}
+              id={column.id}
+            />
             {data.length > MIN_COLUMNS && (
               <Tippy content={"Supprimer la colonne"}>
                 <button
@@ -248,7 +100,7 @@ function MultiColumnsComponent({
             <Tippy content={"Ajouter une colonne"}>
               <button
                 key={`${column.id}-add-block`}
-                className="px-3 py-2 border border-dashed bg-gray-50 focus:outline-none hover:bg-gray-100"
+                className="px-3 py-2 mt-2 border border-dashed bg-gray-50 focus:outline-none hover:bg-gray-100"
                 onClick={() => addColumn(indexColumn)}
               >
                 <i className="fa fa-plus"></i>
