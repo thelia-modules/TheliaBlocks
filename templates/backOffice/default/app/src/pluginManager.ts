48662501e315d9360cd59a48b0dbd032b66339d6
Damien Foulhoux <dfoulhoux@openstudio.fr>
2021-04-30 10:57:07 +0200

    add base for button block

--- templates/backOffice/default/app/src/pluginManager.ts
+++ templates/backOffice/default/app/src/pluginManager.ts
@@ -1,10 +1,11 @@
+import Button from "./blocks/Button";
 import ImageBlock from "./blocks/Image";
 import MultiColumnss from "./blocks/MultiColumns";
 import Product from "./blocks/Product";
+import Separator from "./blocks/Separator";
 import Text from "./blocks/Text";
 import Title from "./blocks/Title";
 import Video from "./blocks/Video";
-import Separator from "./blocks/Separator";
 import { nanoid } from "nanoid";
 // const modules = import.meta.globEager("./blocks/*.tsx");
 
@@ -16,6 +17,7 @@ export const __PLUGINS = [
   { id: nanoid(), ...Title },
   { id: nanoid(), ...Video },
   { id: nanoid(), ...Separator },
+  { id: nanoid(), ...Button },
 ];
 
 export function registerPlugin(plugin: any) {
