ff0264f58acb7ffc4a0eb9e1b58606b70406cacb
Antoine ROYER <aroyer@openstudio.fr>
2021-04-30 17:57:53 +0200

    add highlight bloc

---
+++ templates/backOffice/default/app/src/blocks/Highlight.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { nanoid } from "nanoid";
+
+import { BlockModuleComponentProps, BlockPluginDefinition } from "../types";
+import { IBlock } from "../types";
+import BlockText from "./Text";
+
+export type BlockHighlightData = {
+  value: string;
+};
+
+function BlockHighlightComponent({
+  data,
+  onUpdate,
+}: BlockModuleComponentProps<BlockHighlightData>) {
+  const handleUpdate = (newData: IBlock["data"]) => {
+    onUpdate(newData);
+  }
+
+  return (
+    <div className="BlockHighlight">
+      <BlockText.component
+        data={data}
+        onUpdate={handleUpdate}
+        id={nanoid()}
+      />
+    </div>
+  );
+}
+
+const initialData = {
+  value: "",
+};
+
+const moduleType = {
+  id: "blockHighlight",
+};
+
+const BlockHighlight: BlockPluginDefinition<BlockHighlightData> = {
+  type: moduleType,
+  component: BlockHighlightComponent,
+  initialData,
+  title: {
+    default: "Highlight",
+    fr_FR: "Mise en avant",
+  },
+  description: {
+    default: "Display a highlighted text",
+    fr_FR: "Affiche un bloc de texte mis en avant",
+  },
+  image: {
+    default:
+      "https://source.unsplash.com/featured/300x250?nature&blockHighlight",
+  },
+};
+
+export default BlockHighlight;
