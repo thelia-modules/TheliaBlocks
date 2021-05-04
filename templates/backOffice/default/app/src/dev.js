1019e9da3747e2bf1d525c6c9dc3666f1963f213
Antoine ROYER <aroyer@openstudio.fr>
2021-05-04 10:19:57 +0200

    mode dev

---
+++ templates/backOffice/default/app/src/dev.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TheliaBlocksBackOffice from './main';
+
+ReactDOM.render(React.createElement(TheliaBlocksBackOffice), document.getElementById('thelia-blocks-root'));
