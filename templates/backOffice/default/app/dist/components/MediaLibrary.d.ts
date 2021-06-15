6cecf397001ec16bf5d6c5adfd540888b4ff591a
Damien Foulhoux <dfoulhoux@openstudio.fr>
2021-06-15 14:14:10 +0200

    media library is now a separate component

---
+++ templates/backOffice/default/app/dist/components/MediaLibrary.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+import { LibraryImage } from "../types";
+export default function MediaLibrary({ onSelect, limit, }: {
+    onSelect: (image: LibraryImage) => void;
+    limit?: number;
+}): JSX.Element;
+//# sourceMappingURL=MediaLibrary.d.ts.map
