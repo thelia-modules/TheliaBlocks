907e19fa068d52e0732eea2d856e3756a6ca33c3
Damien Foulhoux <dfoulhoux@openstudio.fr>
2022-06-02 15:53:29 +0200

    new integ for v2

--- Controller/Admin/ConfigurationController.php
+++ Controller/Admin/ConfigurationController.php
@@ -1,19 +1,54 @@
 <?php
 
+/*
+ * This file is part of the Thelia package.
+ * http://www.thelia.net
+ *
+ * (c) OpenStudio <info@thelia.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace TheliaBlocks\Controller\Admin;
 
-use TheliaBlocks\TheliaBlocks;
+use Symfony\Component\Routing\Annotation\Route;
 use Thelia\Controller\Admin\BaseAdminController;
 
 /**
- * Class ConfigurationController
- * @package TheliaBlocks\Controller
+ * Class ConfigurationController.
+ *
  * @author Damien Foulhoux <dfoulhoux@openstudio.fr>
+ */
+
+/**
+ * @Route("/admin/TheliaBlocks", name="thelia_blocks")
  */
 class ConfigurationController extends BaseAdminController
 {
-    public function reactAppAction()
+    /**
+     * @Route("", name="_list", methods="GET")
+     */
+    public function reactAppListingAction()
     {
-        return $this->render("thelia-blocks-configuration");
+        return $this->render('thelia-blocks-configuration');
+    }
+
+    /**
+     * @Route("/new", name="_new", methods="GET")
+     */
+    public function reactAppNewAction()
+    {
+        return $this->render('thelia-blocks-new-configuration');
+    }
+
+    /**
+     * @Route("/{blockGroupId}", name="_edit", methods="GET", requirements={"blockGroupId"="\d+"})
+     */
+    public function reactAppEditAction($blockGroupId)
+    {
+        return $this->render('thelia-blocks-item-configuration', [
+            'blockGroupId' => $blockGroupId,
+        ]);
     }
 }
--- templates/backOffice/default/vendor/index.d.ts
+++ templates/backOffice/default/vendor/index.d.ts
@@ -1,5 +1,19 @@
-declare function BlocksEditor({ apiUrl }: {
+declare type Locale = {
+    id: number;
+    title: string;
+    code: string;
+    active: boolean;
+};
+
+declare function BlocksEditor({ apiUrl, containerId, groupId, locales, }: {
+    apiUrl: string;
+    locales: Locale[];
+    containerId: string;
+    groupId?: number;
+}): JSX.Element | null;
+
+declare function BlocksList({ apiUrl }: {
     apiUrl: string;
 }): JSX.Element | null;
 
-export { BlocksEditor };
+export { BlocksEditor, BlocksList };
