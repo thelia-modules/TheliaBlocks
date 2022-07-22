a29c09936a3366fda47b9d053e5a2c3e747cbe8e
Damien Foulhoux <dfoulhoux@openstudio.fr>
2022-07-22 11:56:24 +0200

    add linking capability

--- templates/backOffice/default/vendor/index.d.ts
+++ templates/backOffice/default/vendor/index.d.ts
@@ -23,6 +23,14 @@ declare const BlocksList: ({ apiUrl }: {
     apiUrl: string;
 }) => JSX.Element | null;
 
+interface ILinkBlockToItemprops {
+    apiUrl: string;
+    groupId?: number;
+    itemId?: number;
+    itemType?: string;
+}
+declare const LinkBlockToItem: ({ apiUrl, groupId, itemId, itemType, }: ILinkBlockToItemprops) => JSX.Element | null;
+
 declare function registerPlugin(plugin: any): void;
 
-export { BlocksEditor, BlocksList, fetcher, queryClient, registerPlugin };
+export { BlocksEditor, BlocksList, LinkBlockToItem, fetcher, queryClient, registerPlugin };
